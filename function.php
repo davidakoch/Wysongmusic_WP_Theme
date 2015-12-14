c46ca9c76e0b489de8d92c1652273e46c847ee02
David A Koch <kodave1@gmail.com>
2015-12-14 00:16:45 -0500

    added css and javascript to functions php

--- function.php
+++ function.php
@@ -2,11 +2,19 @@
 
 function wm_theme_styles() {
 
-	wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+	wp_enqueue_style( 'googlefont_css', 'https://fonts.googleapis.com/css?family=Permanent+Marker' );
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
 
 
 }
 add_action( 'wp_enqueue_scripts', 'wm_theme_styles' );
+
+function wm_theme_js() {
+
+	wp_enqueue_script( 'main.js', get_template_directory_uri() . '/main.js', array('jquery'), '', false);
+
+}
+
+add_action( 'wp_enqueue_scripts', 'wm_theme_js' );
 
 ?>
---
+++ main.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function($) {
+
+	$(document).on('ready', function(){
+
+		//makes the 700px menu appear when you hit menu
+		$('.js_click_menu').on('click', function(){
+			$('.main_header').css("height", 500);
+			$('.menu_700px').css("display","block");
+			$('.banner_700px').css("display","none");
+
+
+		});
+
+		//makes the 700px menu banner appear when you hit close
+		$('.js_click_banner').on('click', function(){
+			$('.banner_700px').css("display","block");
+			$('.menu_700px').css("display","none");
+			$('.main_header').css("height", 225);
+		});
+
+		//makes banner 700px disappear when you stretch the screen beyond 700px
+
+		$(window).on('resize', function(){
+			var w = $(window).width(); 
+			if (w > 700 && $('.banner_700px').is(':visible')){
+				$('.banner_700px').removeAttr('style');
+			} 
+		});	
+		
+		//makes menu 700 px disappear when you stretch
+
+		$(window).on('resize', function(){
+			var w = $(window).width();
+			if (w > 680 && $('.menu_700px').is(':visible')){ 
+				$('.menu_700px').removeAttr('style');
+				$('.main_header').css("height", 225);
+			}	
+		});
+
+		//makes banner 700 px reappear 
+
+		$(window).on('resize', function(){
+			var w = $(window).width();
+			if (w < 700 && $('.banner_700px').is(':hidden')){ 
+				$('.banner_700px').removeAttr('style');
+			}
+		});
+
+		$(window).on('resize', function(){
+			var w = $(window).width();
+			if (w < 700 && $('.menu_700px').is(':visible')){ 
+				$('.banner_700px').css("display","none");
+			}
+		});
+
+	});//end of document.on ready
+
+});
