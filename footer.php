8b702cc9859f00684a07841f085bcc63690d243f
David A Koch <kodave1@gmail.com>
2015-12-17 18:07:14 -0500

    added html to header and footer php

--- footer.php
+++ footer.php
@@ -1,1 +1,19 @@
-<p>footer</p>
+<footer class="site_footer_950px">
+			<p>Proudly Powered by WordPress</p>
+			<p>Wysong Music Theme</p>
+			<p>@Copyright 2015</p>
+			<p>David A. Koch</p>
+		</footer>
+		<footer class="site_footer">
+			<div class="wordpress_text">
+				<p>Proudly Powered by WordPress</p>
+			</div>
+			<div class="footer_text_right">
+				<p>Wysong Music Theme</p>
+				<p>@Copyright 2015</p>
+				<p>David A. Koch</p>
+			</div>
+		</footer>
+	</div>
+</body>
+</htmL>
--- header.php
+++ header.php
@@ -1,1 +1,60 @@
-<p>header</p>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php wp_title(); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="scripts/main.js"></script>
+</head>
+<body>
+	<div class="container">
+		<header class="main_header">
+			<nav>
+				<ul class="main_nav">
+					<li class="active">
+						<a>Home</a>
+					</li>
+					<li>
+						<a href="bio.html">Bio</a>
+					</li>
+					<li>
+						<a>Listen</a>
+							<ul class="sub_music_menu">
+								<li><a href="musicOriginal.html">Originals</a></li>
+								<li><a href="musicTvfilm.html">TV/Film</a></li>
+							</ul>
+					</li>	
+					<li><a href="credits.html">Credits</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</nav>
+
+				<!-- Creates the banner heading at 700px --> 
+			<nav>	
+				<ul class="banner_700px">
+					<li class="js_click_menu">MENU</li>
+				</ul>
+			</nav>
+				<!--creates the menu at 700px -->
+			<nav>
+				<ul class="menu_700px">
+					<li class="js_click_banner">CLOSE</li>
+					<li><a href="index.html">HOME</a></li>
+					<li><a href="bio.html"BIO</a></li>
+					<li><a href="musicOriginal.html">LISTEN/ORIGINALS</a></li>
+					<li><a href="musicTvfilm.html">LISTEN/TV + FILM</a></li>
+					<li><a href="credits.html">CREDITS</a></li>
+					<li><a href="contact.html">CONTACT</a></li>
+				</ul>
+			</nav>
+
+				<!--Handles the logo -->
+			<div class="header_logo">
+				<i class="note_logo fa fa-music fa-5x"></i>
+				<h1 class="main_logo">Wysong Music</h1>
+			</div>
+		</header>
