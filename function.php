6cfc7f5e6e7e064e80473e6fad1d1f6667630aec
David A Koch <kodave1@gmail.com>
2015-12-17 22:12:49 -0500

    removed import url google font from style css

--- function.php
+++ function.php
@@ -1,6 +1,6 @@
 <?php 
 
-function wm_theme_styles() {
+function wpwm_theme_styles() {
 
 	wp_enqueue_style( 'googlefont_css', 'https://fonts.googleapis.com/css?family=Permanent+Marker' );
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
@@ -8,14 +8,14 @@ function wm_theme_styles() {
 
 }
 
-add_action( 'wp_enqueue_scripts', 'wm_theme_styles' );
+add_action( 'wp_enqueue_scripts', 'wpwm_theme_styles' );
 
-function wm_theme_js() {
+function wpwm_theme_js() {
 
 	wp_enqueue_script( 'main.js', get_template_directory_uri() . '/main.js', array('jquery'), ' ' , false);
 
 }
 
-add_action( 'wp_enqueue_scripts', 'wm_theme_js' );
+add_action( 'wp_enqueue_scripts', 'wpwm_theme_js' );
 
 ?>
